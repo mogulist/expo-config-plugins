4f469978626e9cae4aa428736dc8b6d1ee810c3f
Herbert Lim <mogulist@naver.com>
2024-06-19 23:05:32 +0900

    Add and setup naver map but map is not showing yet

---
+++ app.config.js
@@ -0,0 +1,40 @@
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    plugins: [
+      "expo-router",
+      "expo-build-properties",
+      [
+        "@mj-studio/react-native-naver-map",
+        {
+          client_id: process.env.EXPO_PUBLIC_NAVER_MAP_CLIENT_ID,
+          // (optional, you can set with expo-location instead of this package)
+          android: {
+            ACCESS_FINE_LOCATION: true,
+            ACCESS_COARSE_LOCATION: true,
+            ACCESS_BACKGROUND_LOCATION: true,
+          },
+          // (optional, you can set with expo-location instead of this package)
+          ios: {
+            NSLocationAlwaysAndWhenInUseUsageDescription:
+              "{{ your location usage description }}",
+            NSLocationWhenInUseUsageDescription:
+              "{{ your location usage description }}",
+            NSLocationTemporaryUsageDescriptionDictionary: {
+              purposeKey: "{{ your purpose key }}",
+              usageDescription: "{{ your location usage description }}",
+            },
+          },
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            extraMavenRepos: ["https://repository.map.naver.com/archive/maven"],
+          },
+        },
+      ],
+    ],
+  };
+};
---
+++ app/(tabs)/map.tsx
@@ -0,0 +1,29 @@
+import { NaverMapView, type Region } from "@mj-studio/react-native-naver-map";
+
+export default function map() {
+  const jejuRegion: Region = {
+    latitude: 33.20530773,
+    longitude: 126.14656715029,
+    latitudeDelta: 0.38,
+    longitudeDelta: 0.8,
+  };
+  const P0 = { latitude: 37.564362, longitude: 126.977011 };
+  const P1 = { latitude: 37.565051, longitude: 126.978567 };
+  const P2 = { latitude: 37.565383, longitude: 126.976292 };
+
+  return (
+    <NaverMapView
+      style={{ flex: 1 }}
+      // showsMyLocationButton={true}
+      onTapMap={(params) => console.warn("onTouch", JSON.stringify(params))}
+      onCameraChanged={(e) => console.warn("onCameraChange", JSON.stringify(e))}
+      initialRegion={{
+        latitude: 33.20530773,
+        longitude: 126.14656715029,
+        latitudeDelta: 0.38,
+        longitudeDelta: 0.8,
+      }}
+      mapType="Basic"
+    ></NaverMapView>
+  );
+}
